8bfd49bb465dded71868d787520c07a38ff5ad0d
Jose Fernando <jofer1923@gmail.com>
2020-04-04 20:49:05 -0500

    se termina con el home en la parte de la API, quedan haciendo ddalta corregir bien el grid de la sección

--- atomic_html/util/index.js
+++ atomic_html/util/index.js
@@ -7,7 +7,7 @@
  */
 let menu = document.getElementById('desplegarMenu');
 let menu2 = document.getElementById('desplegar2');
-let search = document.getElementById('buscar');
+let buscar = document.getElementById('buscar');
 let mostrarRosa = document.getElementById('busqueda__principal--rosa');
 let busquedaInput = document.getElementById('busqueda__principal--input');
 let lupa = document.getElementById('lupa');
@@ -15,18 +15,18 @@ let lupa = document.getElementById('lupa');
 // MENU DESPLEGABLE PARA LOS TEMAS
 
 let text = menu.addEventListener('click', function () {
-  document.getElementById('desplegar').classList.add('desplegar')
-  document.getElementById('desplegar2').classList.add('desplegar')
+  document.getElementById('desplegar').classList.toggle('desplegar')
+  document.getElementById('desplegar2').classList.toggle('desplegar')
 })
 
 let img = menu2.addEventListener('click', function () {
-  document.getElementById('desplegar').classList.add('desplegar')
-  document.getElementById('desplegar2').classList.add('desplegar')
+  document.getElementById('desplegar').classList.toggle('desplegar')
+  document.getElementById('desplegar2').classList.toggle('desplegar')
 })
 
 function ocultar() {
-  document.getElementById('desplegar').classList.remove('desplegar')
-  document.getElementById('desplegar2').classList.remove('desplegar')
+  document.getElementById('desplegar').classList.toggle('desplegar')
+  document.getElementById('desplegar2').classList.toggle('desplegar')
 }
 
 // HOVER PARA LAS OPCIONES DE BUSQUEDA
@@ -46,7 +46,11 @@ let mouseout = opcion.addEventListener('mouseout', event => {
 
 // CAPTURAR EL INPUT PARA MOSTAR LAS OPCIONES DE BUSQUEDA
 
-let mostrar = search.addEventListener('input', () => {
+let mostrar = buscar.addEventListener('input', () => {
+  console.log('Aqui empieso a escribir');
+  console.log(lupa);
+
+
   opcion.classList.add('busqueda__opciones--mostrar')
   mostrarRosa.classList.add('busqueda__principal--borde')
   mostrarRosa.classList.add('busqueda__principal--rosa')
@@ -67,38 +71,15 @@ let hidden = opcion.addEventListener('click', () => {
   busquedaInput.classList.add('busqueda__principal--active--input')
 })
 
-//  CAPTURAR EL NUMERO DE VISITAS A LA PAGINA
-
-// let contadorVisitas = 12765803;
-
-
-
-//  var counter = 12765803;
-
-//  if (acumulador >= counter){
- 
-//    acumulador = acumulador + 1
-
-//  }
-//  alert(acumulador)
- 
-//  let visitas1 = 12765803
-
-// let firstTime = true; // PARA SETEAR SE MODIFICA A FALSE SE ACTUALIZA EL VALOR Y REGRESA A TRUE
-// if (firstTime === false){
-//   localStorage.setItem("counter","12765803");
-
-// }
-
-// document.addEventListener("DOMContentLoaded", aumentarCounter);
-
-// function aumentarCounter(){
-//   let a = Number(localStorage.getItem("counter")) + 1;
-//   alert(a);
-//   localStorage.setItem("counter", String(a));
-//   alert(a);
-// }
-
- 
+let submit = document.getElementById('busqueda__principal--input')
+let hiddenSubmit = submit.addEventListener('click', () => {
+  opcion.classList.remove('busqueda__opciones--mostrar')
+  mostrarRosa.classList.remove('busqueda__principal--borde')
+  mostrarRosa.classList.remove('busqueda__principal--rosa')
+  mostrarRosa.classList.remove('busqueda__principal')
+  mostrarRosa.classList.add('busqueda__principal--rosa--active')
+  busquedaInput.classList.remove('busqueda__principal--rosa')
+  busquedaInput.classList.add('busqueda__principal--active--input')
+})
 
 
